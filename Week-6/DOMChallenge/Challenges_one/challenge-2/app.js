ea68fb8097fc18d32caba8f62bec1569bbc2ebcd
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-03-02 17:13:57 +0530

    Completed DOM second Challenge

--- Week-6/DOMChallenge/Challenges_one/challenge-2/app.js
+++ Week-6/DOMChallenge/Challenges_one/challenge-2/app.js
@@ -1,3 +1,24 @@
 /**
  * Write your challenge solution here
  */
+
+let title = document.getElementById('mainHeading')
+let colors = document.getElementsByClassName('color-buttons')
+let colorsChild = Array.from(colors[0].children)
+
+colorsChild.forEach((child)=>{
+    child.addEventListener('click',()=>{
+        const getColor = child.textContent
+        addColor(getColor)
+    })
+})
+
+
+  function addColor(selectedColor){
+    if(selectedColor == 'Reset'){
+        title.style.color = 'black'
+    }
+    else{
+        title.style.color = selectedColor
+    }
+  }
