4b6d49915806f5f0d8f9763330d0536ee6b63d72
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-03-02 17:59:29 +0530

    Completed DOM Third Challenge

--- Week-6/DOMChallenge/Challenges_one/challenge-3/app.js
+++ Week-6/DOMChallenge/Challenges_one/challenge-3/app.js
@@ -1,3 +1,52 @@
 /**
  * Write your challenge solution here
  */
+
+let name1 = document.getElementById('nameInput');
+let job = document.getElementById('jobInput');
+let age = document.getElementById('ageInput');
+let bio = document.getElementById('bioInput')
+
+let nameDisplay = document.getElementById('nameDisplay')
+let jobDisplay = document.getElementById('jobDisplay');
+let ageDisplay = document.getElementById('ageDisplay');
+let bioDisplay = document.getElementById('bioDisplay');
+
+
+name1.addEventListener('input',(e)=>{
+    let value = e.target.value
+    changeFn(nameDisplay ,value)
+})
+job.addEventListener('input',(e)=>{
+    let value = e.target.value
+    changeFn(jobDisplay ,value)
+})
+age.addEventListener('input',(e)=>{
+    let value = e.target.value
+    changeFn(ageDisplay ,value)
+})
+bio.addEventListener('input',(e)=>{
+    let value = e.target.value
+    changeFn(bioDisplay ,value)
+})
+
+
+function changeFn( AplliedValue,value){
+            if(value.length !== 0){
+        
+                AplliedValue.textContent = value
+            }
+            else{
+                AplliedValue.textContent = "Not provided"
+            }
+}
+
+// name1.addEventListener('input',(e)=>{
+//     if(e.target.value.length !== 0){
+
+//         nameDisplay.textContent = e.target.value
+//     }
+//     else{
+//         nameDisplay.textContent = "Not Provided"
+//     }
+// })
