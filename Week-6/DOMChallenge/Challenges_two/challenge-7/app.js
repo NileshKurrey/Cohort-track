fd64e5c78fa79dd9e78e15d2d21f66ccb708fb48
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-03-03 00:27:35 +0530

    Completed DOM seventh Challenge

--- Week-6/DOMChallenge/Challenges_two/challenge-7/app.js
+++ Week-6/DOMChallenge/Challenges_two/challenge-7/app.js
@@ -1,3 +1,23 @@
 /**
  * Write your challenge solution here
  */
+
+const accordBtn = document.querySelectorAll('.accordion-button');
+const arrow = document.querySelector('.arrow');
+const content = document.getElementsByClassName('.accordion-content')
+
+
+accordBtn.forEach((item)=>{
+    console.log(item.children)
+    item.addEventListener('click',(e)=>{
+       if(!item.parentElement.classList.contains('active')){
+            item.parentElement.classList.add('active')
+            item.children[0].classList.add('active')
+       }
+       else{
+            item.parentElement.classList.remove('active')
+            item.children[0].classList.remove('active')
+       }
+    })
+    
+})
