f0f93516df56bd0f0f358fd933c4d977c449d50d
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-03-03 00:58:07 +0530

    Completed eigth Dom Challenge

--- Week-6/DOMChallenge/Challenges_two/challenge-9/app.js
+++ Week-6/DOMChallenge/Challenges_two/challenge-9/app.js
@@ -1,3 +1,26 @@
 /**
  * Write your challenge solution here
  */
+
+const openBtn = document.querySelector('.toggle-btn');
+const content = document.querySelector('.content');
+const closeBtn = document.querySelector('.close-btn');
+const panel = document.querySelector('.panel')
+
+
+openBtn.addEventListener('click',(e)=>{
+    if(!panel.classList.contains('active')){
+        panel.classList.add('active')
+     
+    }else{
+        panel.classList.remove('active')
+    }
+})
+
+closeBtn.addEventListener('click',(e)=>{
+    
+        panel.classList.remove('active')
+    
+})
+
+
